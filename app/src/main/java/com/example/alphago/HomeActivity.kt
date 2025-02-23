daa22f1ce55d092b8557d38824e07b60c2a8e8ec
Lore-03 <alurrego03@gmail.com>
2025-02-23 15:31:07 -0500

    primera copia materia aplicaciones nativas

---
+++ app/src/main/java/com/example/alphago/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.alphago
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/alphago/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alphago
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(saveInstanceState: Bundle?){
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
